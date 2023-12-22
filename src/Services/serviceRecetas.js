b35520228b4c9957036f82080cf4782959de63d3
DayannaRojas123 <112741251+DayannaRojas123@users.noreply.github.com>
2023-12-22 02:09:14 -0500

    storage serviceRecetas.js

--- src/Services/serviceRecetas.js
+++ src/Services/serviceRecetas.js
@@ -1,7 +1,16 @@
 const { Recetas, Fermentables, LevadurasReceta, LupulosReceta, AdicionesReceta, conn } = require('../db')
 const cloudinary = require('cloudinary').v2
 const multer = require('multer')
-const upload = multer({ dest: 'uploads' })
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+})
+const upload = multer({storage: storage })
 const fs = require('fs')
 
 cloudinary.config({
